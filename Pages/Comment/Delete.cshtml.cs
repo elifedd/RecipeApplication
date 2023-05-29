11ebee535aa3340c2dd7204e1c3840be77a9ded9
elifedd <elif.demircioglu@icloud.com>
2023-05-29 23:21:47 +0300

    Add CRUD for Comment

---
+++ Pages/Comment/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RecipeApp.Models;
+
+namespace RecipeApp.Pages.Comment
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RecipeApp.Models.RecipeAppContext _context;
+
+        public DeleteModel(RecipeApp.Models.RecipeAppContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Models.Comment Comment { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Comments == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comments.FirstOrDefaultAsync(m => m.CommentId == id);
+
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Comment = comment;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Comments == null)
+            {
+                return NotFound();
+            }
+            var comment = await _context.Comments.FindAsync(id);
+
+            if (comment != null)
+            {
+                Comment = comment;
+                _context.Comments.Remove(Comment);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
