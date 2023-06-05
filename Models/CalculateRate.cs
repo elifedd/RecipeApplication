2c2806c4991ce859a76b12ac40e4ade90176dbab
elifedd <elif.demircioglu@icloud.com>
2023-06-05 11:27:29 +0300

    Add rating feature to the recipe cards

---
+++ Models/CalculateRate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+
+namespace RecipeApp.Models
+{
+    public class CalculateRate
+    {
+        public static double GetRating(List<int> rates)
+        {
+            if(rates == null || rates.Count == 0)
+            {
+                return 0;
+            }
+
+            int star5 = rates.Count(x => x == 5);
+            int star4 = rates.Count(x => x == 4);
+            int star3 = rates.Count(x => x == 3);
+            int star2 = rates.Count(x => x == 2);
+            int star1 = rates.Count(x => x == 1);
+
+            double rating = (double)(star5 * 5 + star4 * 4 + star3 * 3 + star2 * 2 + star1 * 1) / (star5 + star4 + star3 + star2 + star1);
+
+            return rating;
+        }
+    }
+}
--- Pages/Recipe/Index.cshtml.cs
+++ Pages/Recipe/Index.cshtml.cs
@@ -23,10 +23,41 @@ namespace RecipeApp.Pages.Recipe
 
         public IList<Models.Recipe> Recipe { get;set; } = default!;
 
+        public List<double> RecipeAverageRatingList { get; set; } = default!;
+
         public string userId { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
+
+        public async Task CalculateRatings()
+        {
+            var AllRecipes = await (from re in _context.Recipes
+                                     select re).ToListAsync();
+
+            var AllRatings = await (from ra in _context.Ratings
+                                     select ra).ToListAsync();
+
+            RecipeAverageRatingList = new List<double>();
+
+            foreach( var item in AllRecipes)
+            {
+                List<int> reciperates = await (from x in _context.Ratings
+                                                where x.RecipeId == item.RecipeId
+                                                select x.Value).ToListAsync();
+
+                if(reciperates.Count >= 1)
+                {
+                    double avg = CalculateRate.GetRating(reciperates);
+                    avg = Math.Round(avg, 1);
+                    RecipeAverageRatingList.Add(avg);
+                }
+                else
+                {
+                    RecipeAverageRatingList.Add(0.0);
+                }
+            }
+        }
 
         public async Task OnGetAsync()
         {
@@ -41,6 +72,7 @@ namespace RecipeApp.Pages.Recipe
                 else
                 {
                     Recipe = await _context.Recipes.ToListAsync();
+                    await CalculateRatings();
                 }
             }
         }
